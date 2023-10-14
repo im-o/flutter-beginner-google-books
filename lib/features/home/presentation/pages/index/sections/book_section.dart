2391f4ec918828c80c8aa7c5cb0a8196d401abe6
rivaldy <frizgt@gmail.com>
2023-10-14 21:42:40 +0800

    :label: book item card to show on list

---
+++ lib/features/home/presentation/pages/index/sections/book_section.dart
@@ -0,0 +1,18 @@
+/// Created by github.com/im-o on 10/14/2023.
+
+import 'package:flutter/material.dart';
+
+class BookSection extends StatefulWidget {
+  const BookSection({super.key});
+
+  @override
+  State<BookSection> createState() => _BookSectionState();
+}
+
+class _BookSectionState extends State<BookSection> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
